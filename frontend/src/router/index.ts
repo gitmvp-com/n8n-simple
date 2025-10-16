7a7f0a3fdcf31019c06df6d96dd410876811d36a
Fili <126945152+filiksyos@users.noreply.github.com>
2025-10-16 11:17:04 +0300

    Add frontend styling, routing, types and API client

---
+++ frontend/src/router/index.ts
@@ -0,0 +1,28 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+import Editor from '../views/Editor.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home,
+  },
+  {
+    path: '/editor',
+    name: 'Editor',
+    component: Editor,
+  },
+  {
+    path: '/workflow/:id',
+    name: 'WorkflowEditor',
+    component: Editor,
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
+export default router
